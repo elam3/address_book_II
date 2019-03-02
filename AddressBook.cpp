9de871e8cded744f51d8a002a07f2b724ec627d1
elam3 <elam3@mail.ccsf.edu>
2019-03-02 15:44:18 -0800

    added struct Person

--- AddressBook.cpp
+++ AddressBook.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+struct Person {
+    string firstname;
+    string lastname;
+    string phone;
+    string address;
+    struct Person* next;
+};
+
 int main() {
     return 0;
 }
